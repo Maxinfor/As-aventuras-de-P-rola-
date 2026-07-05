9020972d90bff3c141232ef05d486139b16c603b
Max <130712338+Maxinfor@users.noreply.github.com>
2026-07-05 13:38:50 -0300

    Update game.js

--- game.js
+++ game.js
@@ -1,15 +1,45 @@
-function create() {
-    // ... (Código anterior de carregar sprite e plataformas) ...
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: { gravity: { y: 800 }, debug: true }
+    },
+    scene: { preload, create, update }
+};
 
-    // Criando a colisão entre a Pérola e as plataformas
+const game = new Phaser.Game(config);
+
+function preload() {
+    // Carregando a sprite sheet com as medidas calculadas
+    this.load.spritesheet("perola", "perola.png", { frameWidth: 96, frameHeight: 105 });
+    this.load.image("chao", "Chao.jpg");
+    this.load.image("plataforma", "plataforma.jpg");
+}
+
+function create() {
+    // Cenário
+    this.plataformas = this.physics.add.staticGroup();
+    this.plataformas.create(400, 568, 'chao').setScale(2).refreshBody();
+    this.plataformas.create(600, 400, 'plataforma');
+    this.plataformas.create(50, 250, 'plataforma');
+
+    // Personagem
+    this.perola = this.physics.add.sprite(100, 450, "perola");
+    this.perola.setCollideWorldBounds(true);
     this.physics.add.collider(this.perola, this.plataformas);
 
-    // Configurando os controles
+    // Animações
+    this.anims.create({ key: "correrDir", frames: this.anims.generateFrameNumbers("perola", { start: 0, end: 7 }), frameRate: 10, repeat: -1 });
+    this.anims.create({ key: "correrEsq", frames: this.anims.generateFrameNumbers("perola", { start: 8, end: 15 }), frameRate: 10, repeat: -1 });
+    this.anims.create({ key: "pular", frames: this.anims.generateFrameNumbers("perola", { start: 16, end: 17 }), frameRate: 5 });
+
     this.cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update() {
-    // Movimentação lateral
+    // Movimentação
     if (this.cursors.left.isDown) {
         this.perola.setVelocityX(-160);
         this.perola.play("correrEsq", true);
@@ -21,20 +51,9 @@ function update() {
         this.perola.anims.stop();
     }
 
-    // Lógica do Pulo
-    // 'this.perola.body.touching.down' garante que ela só pule se estiver no chão
+    // Pulo
     if (this.cursors.up.isDown && this.perola.body.touching.down) {
-        
-        // Pulo Alto (força maior no eixo Y)
-        if (this.cursors.shift.isDown) {
-            this.perola.setVelocityY(-500); 
-        } 
-        // Pulo Baixo (força menor)
-        else {
-            this.perola.setVelocityY(-300);
-        }
-        
-        // Toca a animação de pulo
+        this.perola.setVelocityY(this.cursors.shift.isDown ? -500 : -300);
         this.perola.play("pular", true);
     }
 }
